5cc24832a5ffcf62a2799ad50c3936ca474d830e
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2023-04-25 15:57:59 +0200

    feat: add Star and score

--- src/main.rs
+++ src/main.rs
@@ -92,9 +92,11 @@ fn enemy_movement(mut enemy_query: Query<(&mut Transform, &Enemy)>, time: Res<Ti
     }
 }
 
-fn change_enemy_movement(
+fn update_enemy_direction(
     mut enemy_query: Query<(&Transform, &mut Enemy)>,
     window_query: Query<&Window, With<PrimaryWindow>>,
+    audio: Res<Audio>,
+    asset_loader: Res<AssetServer>,
 ) {
     let window = window_query
         .get_single()
@@ -107,18 +109,27 @@ fn change_enemy_movement(
     let y_maximum = window.height() - half_enemy_size;
 
     for (transform, mut enemy) in enemy_query.iter_mut() {
+        let mut is_direction_changed: bool = false;
+
         let translation = transform.translation;
         if translation.x < minimum || translation.x > x_maximum {
             enemy.direction.x *= -1.;
+            is_direction_changed = true;
         }
         if translation.y < minimum || translation.y > y_maximum {
             enemy.direction.y *= -1.;
+            is_direction_changed = true;
+        }
+
+        if is_direction_changed {
+            let sound_effect = asset_loader.load("audio/impact/impactSoft_medium_000.ogg");
+            audio.play(sound_effect);
         }
     }
 }
 
 fn confine_enemy_movement(
-    mut enemy_query: Query<(&mut Transform, &Enemy)>,
+    mut enemy_query: Query<&mut Transform>,
     window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
     let window = window_query
@@ -126,7 +137,7 @@ fn confine_enemy_movement(
         .with_context(|| "Cannot find window")
         .unwrap();
 
-    for (mut transform, enemy) in enemy_query.iter_mut() {
+    for mut transform in enemy_query.iter_mut() {
         let half_enemy_size = ENEMY_SPRITE_SIZE / 2.;
         let minimum = 0. + half_enemy_size;
         let x_maximum = window.width() - half_enemy_size;
@@ -146,6 +157,34 @@ fn confine_enemy_movement(
 
         if transform.translation.y > y_maximum {
             transform.translation.y = y_maximum
+        }
+    }
+}
+
+fn enemy_hit_player(
+    mut commands: Commands,
+    mut player_query: Query<(Entity, &Transform), With<Player>>,
+    enemy_query: Query<&Transform, With<Enemy>>,
+    asset_server: Res<AssetServer>,
+    audio: Res<Audio>,
+    time: Res<Time>,
+    score: Res<Score>,
+) {
+    if let Ok((player_entity, player_transform)) = player_query.get_single_mut() {
+        for enemy_transform in enemy_query.iter() {
+            let distance = player_transform
+                .translation
+                .distance(enemy_transform.translation);
+            let player_radius = PLAYER_SPRITE_SIZE / 2.;
+            let enemy_radius = ENEMY_SPRITE_SIZE / 2.;
+            if distance < player_radius + enemy_radius {
+                if time.elapsed_seconds() > 2.5 {
+                    println!("Game over, score: {}", score.value.to_string());
+                    let explosion_sound = asset_server.load("audio/sci-fi/explosionCrunch_000.ogg");
+                    audio.play(explosion_sound);
+                    commands.entity(player_entity).despawn();
+                }
+            }
         }
     }
 }
@@ -181,7 +220,6 @@ fn player_movement(
             direction += direction.normalize();
         }
 
-        println!("{:?}", direction);
         transform.translation += direction * PLAYER_MOVEMENT_SPEED * time.delta_seconds();
     }
 }
@@ -219,17 +257,102 @@ fn confine_player_movement(
     }
 }
 
+const TOTAL_STARS: u32 = 10;
+
+#[derive(Component)]
+struct Star {}
+
+fn spawn_stars(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query
+        .get_single()
+        .with_context(|| "Cannot find primary window")
+        .unwrap();
+
+    let star_range = 0..TOTAL_STARS;
+
+    star_range.for_each(|_| {
+        let star_x = random::<f32>() * window.height();
+        let star_y = random::<f32>() * window.width();
+
+        let asset = asset_server.load("sprites/ball/star.png");
+
+        let sprite = SpriteBundle {
+            transform: Transform::from_xyz(star_x, star_y, 0.),
+            texture: asset,
+            ..default()
+        };
+
+        let star = Star {};
+
+        commands.spawn((sprite, star));
+    })
+}
+
+const STAR_SPRITE_SIZE: f32 = 30.;
+
+fn player_hit_star(
+    mut commands: Commands,
+    star_entity_query: Query<(Entity, &Transform), With<Star>>,
+    player_query: Query<&Transform, With<Player>>,
+    asset_server: Res<AssetServer>,
+    audio: Res<Audio>,
+    mut score: ResMut<Score>,
+) {
+    if let Ok(player_transform) = player_query.get_single() {
+        for (star_entity, star_transform) in star_entity_query.iter() {
+            let player_half_size = PLAYER_SPRITE_SIZE / 2.;
+            let star_half_size = STAR_SPRITE_SIZE / 2.;
+            let distance = star_transform
+                .translation
+                .distance(player_transform.translation);
+
+            if distance < player_half_size + star_half_size {
+                score.value += 1;
+                let hit_sound = asset_server.load("audio/ui/tick_001.ogg");
+                audio.play(hit_sound);
+                commands.entity(star_entity).despawn();
+            }
+        }
+    }
+}
+
+#[derive(Resource)]
+struct Score {
+    value: u32,
+}
+
+impl Default for Score {
+    fn default() -> Self {
+        Self { value: 0 }
+    }
+}
+
+fn update_score(score: Res<Score>) {
+    if score.is_changed() {
+        println!("Score: {}", score.value.to_string());
+    }
+}
+
 fn main() {
     App::new()
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_enemy)
+        .add_startup_system(spawn_stars)
         .add_startup_system(spawn_camera)
         .add_plugins(DefaultPlugins)
         .add_plugin(EditorPlugin::default())
+        .init_resource::<Score>()
         .add_system(player_movement)
         .add_system(confine_player_movement)
         .add_system(enemy_movement)
-        .add_system(change_enemy_movement)
+        .add_system(update_enemy_direction)
         .add_system(confine_enemy_movement)
+        .add_system(enemy_hit_player)
+        .add_system(player_hit_star)
+        .add_system(update_score)
         .run();
 }
